e7221ad5cdf8e527fd378030201718e477be539a
Valentin <121805462+vstbg@users.noreply.github.com>
2024-05-14 12:39:09 +0300

    Create main.tf
    Create a basic EC2 instance in AWS.

---
+++ terraform/script-1/main.tf
@@ -0,0 +1,8 @@
+ provider "aws" {
+     region = "us-east-1"  # Set the AWS region
+   }
+
+   resource "aws_instance" "example" {
+     ami           = "ami-0bb84b8ffd87024d8"  # Specify the AMI ID
+     instance_type = "t2.micro"
+   }
