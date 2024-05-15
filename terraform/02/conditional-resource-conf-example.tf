b37ca5bef1a6c8087213ceb78ee2ca82ee3b4625
Valentin <121805462+vstbg@users.noreply.github.com>
2024-05-15 15:06:05 +0300

    Create conditional-resource-conf-example.tf

---
+++ terraform/02/conditional-resource-conf-example.tf
@@ -0,0 +1,11 @@
+resource "aws_security_group" "example" {
+  name = "example-sg"
+  description = "Example security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.enable_ssh ? ["0.0.0.0/0"] : []
+  }
+}
