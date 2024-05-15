9533f7f7e5a1b70fa0a9e8b1ce8e0bbd6868ebc7
Valentin <121805462+vstbg@users.noreply.github.com>
2024-05-15 14:04:17 +0300

    Create multi-region-example.tf
    Spawn instances in multiple regions.

---
+++ terraform/02/multi-region-example.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  alias = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias = "us-west-2"
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-0123456789abcdef0"
+  instance_type = "t2.micro"
+  provider = "aws.us-east-1"
+}
+
+resource "aws_instance" "example2" {
+  ami = "ami-0123456789abcdef0"
+  instance_type = "t2.micro"
+  provider = "aws.us-west-2"
+}
