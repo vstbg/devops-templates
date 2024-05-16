d849d399c2aa2f2ead3ff05bb60fb2470cf458c8
Valentin <121805462+vstbg@users.noreply.github.com>
2024-05-16 14:27:57 +0300

    Create main.tf

---
+++ terraform/03/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami_value = "ami-053b0d53c279acc90" # replace this
+  instance_type_value = "t2.micro"
+  key_name = "my-key" # replace this
+}
