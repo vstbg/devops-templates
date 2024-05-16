59bff3bfbd8b602ef3f3121393f77456b4403619
Valentin <121805462+vstbg@users.noreply.github.com>
2024-05-16 14:25:03 +0300

    Create variables.tf

---
+++ terraform/03/project-ec2/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_value" {
+    description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance_type"
+}
+
+variable "key_name" {
+    description = "value for the key"
+}
