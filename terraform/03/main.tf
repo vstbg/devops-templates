b7424943d473e127892d78b3a3a46a28d349fdf5
Valentin <121805462+vstbg@users.noreply.github.com>
2024-05-16 20:39:05 +0300

    Update main.tf

--- terraform/03/main.tf
+++ terraform/03/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Using ready module
 module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-053b0d53c279acc90" # replace this
