576cb7e692eee50c045bf089a0f2ef7e052c99b7
Valentin <121805462+vstbg@users.noreply.github.com>
2024-09-23 11:39:50 +0300

    Create vars.tf

---
+++ terraform/dove-5/vars.tf
@@ -0,0 +1,19 @@
+variable "REGION" {
+  default = "us-east-2"
+}
+
+variable "ZONE1" {
+  default = "us-east-2a"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-2 = "ami-069d73f3235b535bd"
+    us-east-1 = "ami-06ca3ca175f37dd66"
+  }
+}
+
+variable "USER" {
+  default = "ec2-user"
+}
