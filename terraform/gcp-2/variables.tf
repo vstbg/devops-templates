470bef4bf0b8cf77203700bcc1ed8090c9f97942
Valentin <121805462+vstbg@users.noreply.github.com>
2024-10-30 15:19:39 +0200

    Create variables.tf

---
+++ terraform/gcp-2/variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+  default     = "your-project-id"
+
+}
+
+variable "name" {
+  description = "Name of the buckets to create."
+  type        = string
+  default     = "your-project-id"
+}
