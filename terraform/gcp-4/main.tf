be20535e728c36b547797b505e93dd819adeb122
Valentin <121805462+vstbg@users.noreply.github.com>
2024-11-05 11:54:29 +0200

    Update main.tf

--- terraform/gcp-4/main.tf
+++ terraform/gcp-4/main.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "4.56.0"
+    }
+  }
+}
 
+provider "google" {
+    project = var.project_id
+    region = var.region
+    zone = var.zone
+}
+
+module "instance" {
+  source = "./modules/instances"
+}
+
+module "storage" {
+  source = "./modules/storage"
+}
