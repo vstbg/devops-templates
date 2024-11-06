5fe238599f09c152dc3a2472c364685de22147aa
Valentin <121805462+vstbg@users.noreply.github.com>
2024-11-06 11:51:38 +0200

    Update variables.tf

--- terraform/gcp-4/modules/instances/variables.tf
+++ terraform/gcp-4/modules/instances/variables.tf
@@ -1,1 +1,11 @@
+variable "region" {
+  default = ""                                                          # Enter your region
+}
 
+variable "zone" {
+  default = ""                                                          # Enter your zone
+}
+
+variable "project_id" {                                                 
+  default = ""                                                          # Enter your project
+}
