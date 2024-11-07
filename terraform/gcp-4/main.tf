7227c6906acc836901be67fa26cf1b6987161cad
Valentin <121805462+vstbg@users.noreply.github.com>
2024-11-07 10:00:52 +0200

    Update main.tf

--- terraform/gcp-4/main.tf
+++ terraform/gcp-4/main.tf
@@ -12,9 +12,9 @@ terraform {
 }
 
 provider "google" {
-    project = var.project_id
-    region = var.region
-    zone = var.zone
+  project = var.project_id
+  region = var.region
+  zone = var.zone
 }
 
 module "instance" {
@@ -23,4 +23,28 @@ module "instance" {
 
 module "storage" {
   source = "./modules/storage"
+}
+
+module "vpc" {
+  source = "terraform-google-modules/network/google"
+  version = "~>6.0.0.0"
+
+  project_id = var.project_id
+  network_name = "tf-vpc-982777"
+  routing_mode = "GLOBAL"
+
+  subnets = [
+    {
+      subnet_name = "subnet-01"
+      subnet_ip = "10.10.10.0/24"
+      subnet_region = var.region
+    },
+    {
+      subnet_name = "subnet-02"
+      subnet_ip = "10.10.20.0/24"
+      subnet_region = var.region
+      subnet_private_access = "true"
+      subnet_flow_logs = "true"
+    }
+  ]
 }
